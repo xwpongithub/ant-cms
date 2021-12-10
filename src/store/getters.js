603f9164a4a8e7131cea5afbc968ccc5efcd2b36
Xiao <ja_xwp@yeah.net>
2021-12-10 22:43:46 +0900

    fix(默认头像回显问题): 头像没有时报错

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,7 @@ import variables from '@/styles/js-variables.scss'
 
 export default {
   cssVars: () => variables,
+  menuOpenKeys: state => state.app.menuOpenKeys,
   sidebarCollapsed: state => state.app.sidebarCollapsed,
   tagsViewList: state => state.app.tagsViewList
 }
